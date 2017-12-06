8ffc0e8da8e7b2cb3ac0b8456e12f64b2ecfcb05
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-05 17:53:23 -0800

    change calculator simpile calculator

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,25 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nick=JOptionPane.showInputDialog(null,"how many nickels do you have");
 		// Convert their answer to an int using Integer.parseInt()
-
+int nickal=Integer.parseInt(nick);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dime=JOptionPane.showInputDialog(null,"how many dimes do you have");
+int dimes=Integer.parseInt(dime);		
 		// Ask the user how many quarters they have, and convert their answer
-
+String quart=JOptionPane.showInputDialog(null,"how many quarters do you have");
+int quarters=Integer.parseInt(quart);		
 		// Calculate how much money the user has and save it in a double variable 
+double n=nickal*0.05;
 
+double d=dimes*0.1;
+
+double q=quarters*0.25;
+
+double todal= n+d+q;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "you have $"+todal);
 	}
 }
 
--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,25 +1,75 @@
 
-
 import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
+for (int i = 0; i < 10; i--) {
+	
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+String numa =JOptionPane.showInputDialog(null, "give me a number");
+String numb =JOptionPane.showInputDialog(null, "give me another number");
+double num1=Double.parseDouble(numa);
+double num2=Double.parseDouble(numb);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
 		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "add", "subtract", "mytiply", "divide" },
 				null);
 
+		
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+		if (operation==0) {
+		add(num1, num2);	
+		}else if (operation==1) {
+			sub(num1, num2);	
+		}else if (operation==3) {
+			div(num1, num2);	
+		}else if (operation==2) {
+			mti(num1, num2);	
+		}
+		
+		
+		
+		
+		
+}
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
+	
+}
+	// 3. Create method for addition operation.
+	// Method should create pop-up that shows equation and solution, eg. 45 + 25 =
+	// 70.
+	// HINT: use 'static void add(int num1, num2) { ... }
+public static void add(double num1, double num2) {
+	
+double l = num1 + num2;
+JOptionPane.showMessageDialog(null, l);	
+}
+	// 4. Create similar methods for subtraction, multiplication and division.
+public static void sub(double num1, double num2) {
+
+	double sub = num1 - num2;
+	JOptionPane.showMessageDialog(null, sub);	
+
+
+
+}
+	public static void div(double num1, double num2) {
+
+		double div = num1 / num2;
+		JOptionPane.showMessageDialog(null, div);	
+
 	}
 
-	// 3. Create method for addition operation.
-	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
-	//    HINT: use 'static void add(int num1, num2) { ... }
-
-	// 4. Create similar methods for subtraction, multiplication and division.
+		public static void mti(double num1, double num2) {
+			double mil = num1 * num2;
+			JOptionPane.showMessageDialog(null, mil);	
 }
+}		
+		
+		
+		
+		
+		
+		
+		
