37d39b6c7973b94b92d9cc19ca9487d4fe2a4af8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-09 17:54:40 -0800

    sleepyhead pialoud happy pet

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -5,23 +5,27 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String pi = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912983367336244065664308602139494639522473719070217986094370277053921717629317675238467";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+ for (int x=0; x<pi.length();x++) {
+speak(pi.charAt(x));
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
 
-	// 5. Use the speak() method to speak all the digits of Pi.
+	
 
-	// [ADVANCED]
-	// *6. Get a character from the user using the getInputFromUser() method
-	// *7. Compare the users' char to the next digit of Pi
-	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
+// 5. Use the speak() method to speak all the digits of Pi.
+ }} 
 
+	// [ADVA}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,28 +1,35 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /*
-         * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
-         */
-    
+		boolean isWeekday, isVacation;
 
+		/*
+		 * Ask the user for these values using JOptionPane.showConfirmDialog(null,
+		 * "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		 */
 
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
+		int yes = JOptionPane.showConfirmDialog(null, "is it a weekday?", "hi", JOptionPane.YES_NO_OPTION);
+		int vac = JOptionPane.showConfirmDialog(null, "are you on vacation?", "hi", JOptionPane.YES_NO_OPTION);
+		if (yes == JOptionPane.NO_OPTION) {
+			JOptionPane.showMessageDialog(null, "sleep in");
+		}
+		if (yes == JOptionPane.YES_OPTION) {
+		if (vac == JOptionPane.YES_OPTION) {	
+			JOptionPane.showMessageDialog(null, "sleep in");
+		}else
+		JOptionPane.showMessageDialog(null, "get up lazy bones");
+		}
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!” If it is a weekday, and we are on vacation, print “sleep
+		 * in”.
+		 */
+	}
 }
